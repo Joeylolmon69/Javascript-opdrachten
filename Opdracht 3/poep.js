bbac949133161cdc059ae79129bdde1597ebf175
Joeylolmon69 <106911926+Joeylolmon69@users.noreply.github.com>
2025-11-26 04:24:49 +0100

    Add files via upload

---
+++ Opdracht 3/poep.js
@@ -0,0 +1,3 @@
+  let name = prompt("Wat is je naam?");
+console.log("gebruikersnaam is " + name);
+document.getElementById("naam").textContent = "Hallo " + name + ", welkom bij mijn eerste JavaScript-pagina!";
