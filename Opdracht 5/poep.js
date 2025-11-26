e3daf9f3faeececc59394a131fb41f2f33f1706a
Joeylolmon69 <106911926+Joeylolmon69@users.noreply.github.com>
2025-11-26 05:08:19 +0100

    Add files via upload

---
+++ Opdracht 5/poep.js
@@ -0,0 +1,10 @@
+let stringie = "20"; 
+let getal = 10;
+let getal2 = 5;  
+
+
+let Getallen = getal + getal2;
+document.getElementById("optel-getallen").innerHTML = "Optellen getallen: <span style='color: red;'>" + Getallen + "</span>";
+
+let StringGetal = stringie + getal;
+document.getElementById("optel-string-getal").innerHTML = "Optellen string en getal: <span style='color: red;'>" + StringGetal + "</span>";
